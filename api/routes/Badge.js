9b98087bfc9aaccc96f10c4924776f818c67e0b6
Syahrul Arifuddin Kholid <sayamanusia2000@gmail.com>
2018-09-24 15:06:29 +0700

    Adding development environtment

---
+++ api/routes/Badge.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET req to /badges'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const badge = {
+        badge_name: req.body.badge_name,
+        badge_image: req.body.badge_image
+    };
+    res.status(201).json({
+        message: 'Handling POST req to /badges',
+        createdBadge: badge
+    });
+});
+
+router.get('/:badgeId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'Special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:badgeId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Badge updated!'
+    })
+});
+
+router.delete('/:badgeId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Badge deleted!'
+    });
+});
+
+module.exports = router;
