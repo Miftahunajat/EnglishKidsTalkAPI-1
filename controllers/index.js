381791de792e5057e2177ee5c99cff8c0283f4e9
Syahrul Arifuddin Kholid <sayamanusia2000@gmail.com>
2018-09-29 14:06:43 +0700

    Adding QuestionCategory and QuestionDifficulty REST API

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const BadgeController = require('./BadgeController');
 const AnswerController = require('./AnswerController');
 const ItemCategoryController = require('./ItemCategoryController');
+const QuestionCategoryController = require('./QuestionCategoryController');
+const QuestionDifficultyController = require('./QuestionDifficultyController');
 
 module.exports = {
 	BadgeController,
 	AnswerController,
-	ItemCategoryController
+	ItemCategoryController,
+	QuestionCategoryController,
+	QuestionDifficultyController
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const badgeController = require('../controllers').BadgeController;
 const answerController = require('../controllers').AnswerController;
 const itemCategoryController = require('../controllers').ItemCategoryController;
+const questionCategoryController = require('../controllers').QuestionCategoryController;
+const questionDifficultyController = require('../controllers').QuestionDifficultyController;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -33,5 +35,19 @@ router.get('/api/item-categories/:id', itemCategoryController.getById);
 router.post('/api/item-categories', itemCategoryController.add);
 router.put('/api/item-categories/:id', itemCategoryController.update);
 router.delete('/api/item-categories/:id', itemCategoryController.delete);
+
+/* Question Category Router */
+router.get('/api/question-categories', questionCategoryController.list);
+router.get('/api/question-categories/:id', questionCategoryController.getById);
+router.post('/api/question-categories', questionCategoryController.add);
+router.put('/api/question-categories/:id', questionCategoryController.update);
+router.delete('/api/question-categories/:id', questionCategoryController.delete);
+
+/* Question Difficulty Router */
+router.get('/api/question-difficulties', questionDifficultyController.list);
+router.get('/api/question-difficulties/:id', questionDifficultyController.getById);
+router.post('/api/question-difficulties', questionDifficultyController.add);
+router.put('/api/question-difficulties/:id', questionDifficultyController.update);
+router.delete('/api/question-difficulties/:id', questionDifficultyController.delete);
 
 module.exports = router;
