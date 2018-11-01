d5b884a1dc84cc2e5fc79246a4e629ffa6e2cd8d
Syahrul Arifuddin Kholid <sayamanusia2000@gmail.com>
2018-11-01 22:00:33 +0700

    Running text changing

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, function(){
-    console.log('Express server listening on port : ' + port);
+    console.log('Express server is running on port : ' + port);
 });
