13482de28a119ba521356038927a78ad1ef8dff8
Syahrul Arifuddin Kholid <sayamanusia2000@gmail.com>
2018-12-01 15:29:46 +0700

    Refactoring item purchasing and activation API

--- controllers/transactions/ItemTransactionController.js
+++ controllers/transactions/ItemTransactionController.js
@@ -4,89 +4,64 @@ const Inventory = require('../../models').Inventory;
 module.exports = {
 
 	addItem(req, res) {
-		if (!req.body.item_id){
-			res.status(200).send({'msg': 'Field cannot be null!'});
-		} else {
-			return Inventory
-			.findById(req.params.inventory_id)
-			.then((inventory) => {
-				if (!inventory) {
-					return res.status(404).send({
-						message: 'Inventory Not Found',
-					});
-				}
-				Item
-				.findById(req.body.item_id)
-				.then((item) => {
-					if (!item) {
-						return res.status(404).send({
-							message: 'Item Not Found',
-						});
-					}
-					inventory.getUser()
-					.then((associatedUser) => {
-						if (associatedUser.star_gained < item.star)
-							return res.status(400).send({data: 'Star not enough!'});
-						inventory.addItem(item, {through: {is_active: false}})
-						.then(() => {
-							associatedUser.update({
-								star_gained: associatedUser.star_gained - item.star
-							})
-							.then(() => {
-								inventory.getItems({
-									where: {
-										id: parseInt(req.body.item_id)
-									}
-								})
-								.then((associatedItem) => {
-									res.status(200).send(associatedItem);
-								})
-								.catch((error) => res.status(400).send(error));
-							})
-							.catch((error) => res.status(400).send(error));
-						})
-						.catch((error) => res.status(400).send(error));
-					})
-					.catch((error) => res.status(400).send(error));
-				})
-				.catch((error) => res.status(400).send(error));
-			})
-			.catch((error) => res.status(400).send(error));
-		}
+
+		const findInventoryPromise = Inventory.findById(req.params.id);
+		const findItemPromise = Item.findById(req.body.item_id);
+		const findUserPromise = findInventoryPromise.then(inventory => {
+									return inventory.getUser()
+								});
+
+		Promise.all([
+			findInventoryPromise,
+			findItemPromise,
+			findUserPromise
+		])
+		.then(([inventory, item, user]) => {
+			if (inventory && item) {
+				if (user.star_gained < item.star)
+					return res.status(400).send({data: 'Star not enough!'});
+				inventory.addItem(item, {through: {is_active: false}});
+				user.update({star_gained: user.star_gained - item.star});
+				return res.status(200).send({msg: 'Success!'});
+			} else {
+				let message = '';
+				if (!inventory)
+					message = 'Inventory not found !';
+				else if (!item) 
+					message = 'Item not found !';
+				return res.status(404).send({msg: message});
+			}
+		})
+		.catch((error) => res.status(400).send(error));
+
 	},
 
 	activateItem(req, res) {
-		return Inventory
-		.findById(req.params.inventory_id)
-		.then((inventory) => {
-			if (!inventory) {
-				return res.status(404).send({
-					message: 'Inventory Not Found',
-				});
-			}
-			Item
-			.findById(req.params.item_id)
-			.then((item) => {
-				if (!item) {
-					return res.status(404).send({
-						message: 'Item Not Found',
-					});
-				}
+
+		const findInventoryPromise = Inventory.findById(req.params.id);
+		const findItemPromise = Item.findById(req.params.item_id);
+
+		Promise.all([
+			findInventoryPromise,
+			findItemPromise
+		])
+		.then(([inventory, item]) => {
+			if (inventory && item) {
 				inventory.addItem(item, {through: {is_active: req.query.active}})
-				.then(() => {
-					inventory.getItems({
-						where: {
-							id: parseInt(req.params.item_id)
-						}
-					})
-					.then((associatedItem) => {
-						res.status(200).send(associatedItem);
-					})
-					.catch((error) => res.status(400).send(error));	
-				})
-				.catch((error) => res.status(400).send(error));
-			})
-			.catch((error) => res.status(400).send(error));
+				let activationMsg = '';
+				if (req.query.active == 'false') 
+					activationMsg = 'deactivated';
+				else 
+					activationMsg = 'activated';
+				return res.status(200).send({msg: 'Item has ' + activationMsg + ' !'});
+			} else {
+				let message = '';
+				if (!inventory)
+					message = 'Inventory not found !';
+				else if (!item) 
+					message = 'Item not found !';
+				return res.status(404).send({msg: message});
+			}
 		})
 		.catch((error) => res.status(400).send(error));
 	}
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../config/multer.config');
 
-const badgeController = require('../controllers').BadgeController;
-const answerController = require('../controllers').AnswerController;
-const itemCategoryController = require('../controllers').ItemCategoryController;
-const questionCategoryController = require('../controllers').QuestionCategoryController;
-const questionDifficultyController = require('../controllers').QuestionDifficultyController;
-const inventoryController = require('../controllers').InventoryController;
-const userController = require('../controllers').UserController;
-const learningTopicController = require('../controllers').LearningTopicController;
-const itemController = require('../controllers').ItemController;
-const learningItemController = require('../controllers').LearningItemController;
-const challengeController = require('../controllers').ChallengeController;
-const badgeTransactionController = require('../controllers').BadgeTransactionController;
-const challengeTransactionController = require('../controllers').ChallengeTransactionController;
-const difficultyTransactionController = require('../controllers').DifficultyTransactionController;
-const learningTransactionController = require('../controllers').LearningTransactionController;
-const itemTransactionController = require('../controllers').ItemTransactionController;
+const controllers = require('../controllers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,93 +13,93 @@ router.get('/', function(req, res, next) {
 });
 
 /* User Router */
-router.get('/users', userController.list);
-router.get('/users/:id', userController.getById);
-router.patch('/users/:id', upload.none(), userController.update);
-router.delete('/users/:id', userController.delete);
+router.get('/users', controllers.UserController.list);
+router.get('/users/:id', controllers.UserController.getById);
+router.patch('/users/:id', upload.none(), controllers.UserController.update);
+router.delete('/users/:id', controllers.UserController.delete);
 
-router.post('/users/:id/badges', upload.none(), badgeTransactionController.addBadge);
-router.post('/users/:id/learning-items', upload.none(), learningTransactionController.addLearningItem);
-router.post('/users/:id/challenges', upload.none(), challengeTransactionController.addChallenge);
-router.post('/users/:id/question-difficulties', upload.none(), difficultyTransactionController.addQuestionDifficulty);
+router.post('/users/:id/badges', upload.none(), controllers.BadgeTransactionController.addBadge);
+router.post('/users/:id/learning-items', upload.none(), controllers.LearningTransactionController.addLearningItem);
+router.post('/users/:id/challenges', upload.none(), controllers.ChallengeTransactionController.addChallenge);
+router.post('/users/:id/question-difficulties', upload.none(), controllers.DifficultyTransactionController.addQuestionDifficulty);
 
 /* Learning Item Router */
-router.get('/learning-items', learningItemController.list);
-router.get('/learning-items/:id', learningItemController.getById);
-router.post('/learning-items', upload.single("learning_item_image"), learningItemController.add);
-router.patch('/learning-items/:id', upload.single("learning_item_image"), learningItemController.update);
-router.delete('/learning-items/:id', learningItemController.delete);
+router.get('/learning-items', controllers.LearningItemController.list);
+router.get('/learning-items/:id', controllers.LearningItemController.getById);
+router.post('/learning-items', upload.single("learning_item_image"), controllers.LearningItemController.add);
+router.patch('/learning-items/:id', upload.single("learning_item_image"), controllers.LearningItemController.update);
+router.delete('/learning-items/:id', controllers.LearningItemController.delete);
 
 /* Challenge Router */
-router.get('/challenges', challengeController.list);
-router.get('/challenges/:id', challengeController.getById);
-router.post('/challenges', upload.single("challenge_image"), challengeController.add);
-router.patch('/challenges/:id', upload.single("challenge_image"), challengeController.update);
-router.delete('/challenges/:id', challengeController.delete);
+router.get('/challenges', controllers.ChallengeController.list);
+router.get('/challenges/:id', controllers.ChallengeController.getById);
+router.post('/challenges', upload.single("challenge_image"), controllers.ChallengeController.add);
+router.patch('/challenges/:id', upload.single("challenge_image"), controllers.ChallengeController.update);
+router.delete('/challenges/:id', controllers.ChallengeController.delete);
 
 /* Item Router */
-router.get('/items', itemController.list);
-router.get('/items/:id', itemController.getById);
+router.get('/items', controllers.ItemController.list);
+router.get('/items/:id', controllers.ItemController.getById);
 router.post('/items', upload.fields([
     {name: 'image', maxCount: 1},
     {name: 'snippet', maxCount: 1}
-]), itemController.add);
+]), controllers.ItemController.add);
 router.patch('/items/:id', upload.fields([
     {name: 'image', maxCount: 1},
     {name: 'snippet', maxCount: 1}
-]), itemController.update);
-router.delete('/items/:id', itemController.delete);
+]), controllers.ItemController.update);
+router.delete('/items/:id', controllers.ItemController.delete);
 
 /* Item Category Router */
-router.get('/item-categories', itemCategoryController.list);
-router.get('/item-categories/:id', itemCategoryController.getById);
-router.post('/item-categories', upload.single("item_category_image"), itemCategoryController.add);
-router.patch('/item-categories/:id', upload.single("item_category_image"), itemCategoryController.update);
-router.delete('/item-categories/:id', itemCategoryController.delete);
+router.get('/item-categories', controllers.ItemCategoryController.list);
+router.get('/item-categories/:id', controllers.ItemCategoryController.getById);
+router.post('/item-categories', upload.single("item_category_image"), controllers.ItemCategoryController.add);
+router.patch('/item-categories/:id', upload.single("item_category_image"), controllers.ItemCategoryController.update);
+router.delete('/item-categories/:id', controllers.ItemCategoryController.delete);
 
 /* Question Category Router */
-router.get('/question-categories', questionCategoryController.list);
-router.get('/question-categories/:id', questionCategoryController.getById);
-router.post('/question-categories', upload.none(), questionCategoryController.add);
-router.patch('/question-categories/:id', upload.none(), questionCategoryController.update);
-router.delete('/question-categories/:id', questionCategoryController.delete);
+router.get('/question-categories', controllers.QuestionCategoryController.list);
+router.get('/question-categories/:id', controllers.QuestionCategoryController.getById);
+router.post('/question-categories', upload.none(), controllers.QuestionCategoryController.add);
+router.patch('/question-categories/:id', upload.none(), controllers.QuestionCategoryController.update);
+router.delete('/question-categories/:id', controllers.QuestionCategoryController.delete);
 
 /* Question Difficulty Router */
-router.get('/question-difficulties', questionDifficultyController.list);
-router.get('/question-difficulties/:id', questionDifficultyController.getById);
-router.post('/question-difficulties', upload.none(), questionDifficultyController.add);
-router.patch('/question-difficulties/:id', upload.none(), questionDifficultyController.update);
-router.delete('/question-difficulties/:id', questionDifficultyController.delete);
+router.get('/question-difficulties', controllers.QuestionDifficultyController.list);
+router.get('/question-difficulties/:id', controllers.QuestionDifficultyController.getById);
+router.post('/question-difficulties', upload.none(), controllers.QuestionDifficultyController.add);
+router.patch('/question-difficulties/:id', upload.none(), controllers.QuestionDifficultyController.update);
+router.delete('/question-difficulties/:id', controllers.QuestionDifficultyController.delete);
 
 /* Learning Topic Router */
-router.get('/learning-topics', learningTopicController.list);
-router.get('/learning-topics/:id', learningTopicController.getById);
-router.post('/learning-topics', upload.single("learning_topic_image"), learningTopicController.add);
-router.patch('/learning-topics/:id', upload.single("learning_topic_image"), learningTopicController.update);
-router.delete('/learning-topics/:id', learningTopicController.delete);
+router.get('/learning-topics', controllers.LearningTopicController.list);
+router.get('/learning-topics/:id', controllers.LearningTopicController.getById);
+router.post('/learning-topics', upload.single("learning_topic_image"), controllers.LearningTopicController.add);
+router.patch('/learning-topics/:id', upload.single("learning_topic_image"), controllers.LearningTopicController.update);
+router.delete('/learning-topics/:id', controllers.LearningTopicController.delete);
 
 /* Badge Router */
-router.get('/badges', badgeController.list);
-router.get('/badges/:id', badgeController.getById);
-router.post('/badges', upload.single("badge_image") ,badgeController.add);
-router.patch('/badges/:id', upload.single("badge_image"), badgeController.update);
-router.delete('/badges/:id', badgeController.delete);
+router.get('/badges', controllers.BadgeController.list);
+router.get('/badges/:id', controllers.BadgeController.getById);
+router.post('/badges', upload.single("badge_image") ,controllers.BadgeController.add);
+router.patch('/badges/:id', upload.single("badge_image"), controllers.BadgeController.update);
+router.delete('/badges/:id', controllers.BadgeController.delete);
 
 /* Answer Router */
-router.get('/answers', answerController.list);
-router.get('/answers/:id', answerController.getById);
-router.post('/answers', upload.none(), answerController.add);
-router.patch('/answers/:id', upload.none(), answerController.update);
-router.delete('/answers/:id', answerController.delete);
+router.get('/answers', controllers.AnswerController.list);
+router.get('/answers/:id', controllers.AnswerController.getById);
+router.post('/answers', upload.none(), controllers.AnswerController.add);
+router.patch('/answers/:id', upload.none(), controllers.AnswerController.update);
+router.delete('/answers/:id', controllers.AnswerController.delete);
 
 /* Inventory Router */
-router.get('/inventories', inventoryController.list);
-router.get('/inventories/:id', inventoryController.getById);
-router.post('/inventories', inventoryController.add);
-router.patch('/inventories/:id', inventoryController.update);
-router.delete('/inventories/:id', inventoryController.delete);
+router.get('/inventories', controllers.InventoryController.list);
+router.get('/inventories/:id', controllers.InventoryController.getById);
+router.post('/inventories', controllers.InventoryController.add);
+router.patch('/inventories/:id', controllers.InventoryController.update);
+router.delete('/inventories/:id', controllers.InventoryController.delete);
 
-router.post('/inventories/:inventory_id/items', upload.none(), itemTransactionController.addItem);
-router.get('/inventories/:inventory_id/items/:item_id', upload.none(), itemTransactionController.activateItem);
+router.post('/inventories/:id/items', upload.none(), controllers.ItemTransactionController.addItem);
+router.get('/inventories/:id/items/:item_id', upload.none(), controllers.ItemTransactionController.activateItem);
 
 module.exports = router;
